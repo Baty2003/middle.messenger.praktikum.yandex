36b19a012485f926266495de44a69388ad73b561
I am baty <vosf03@gmail.com>
2023-12-23 12:51:51 +0500

    nn

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 console.log(path.join(__dirname, "index.html"));
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(PORT, () => {
   console.log(`Express работает ${PORT}!`);
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import { defineConfig } from "vite";
 import handlebars from "./plugins/vite-plugin-handlebars-precompile";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   plugins: [handlebars()],
 });
