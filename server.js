d9e7729845053cc3e8cb871b9acbf412091fcaaf
I am baty <vosf03@gmail.com>
2023-12-23 12:25:02 +0500

    fix server file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 console.log(path.join(__dirname, "index.html"));
 const PORT = process.env.PORT || 3000;
 const app = express();
